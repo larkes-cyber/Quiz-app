39dfece425847a8d59e66a59b9b17189ad0909cd
larkes-cyber <playegrovoy@gmail.com>
2022-03-05 20:59:52 +0300

    I developed result screen.

--- app/src/main/java/com/example/myapplication/ScreenOfResults.kt
+++ app/src/main/java/com/example/myapplication/ScreenOfResults.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class ScreenOfResults: AppCompatActivity() {
@@ -11,7 +13,12 @@ class ScreenOfResults: AppCompatActivity() {
         setContentView(R.layout.activity_screen_of_results);
         val result:TextView = findViewById(R.id.textOfResults);
         val value = intent.extras!!.getString("value");
-        result.setText(value);
+        result.setText("${value}/10");
+        val button:Button=findViewById(R.id.button);
+        button.setOnClickListener {
+            val intent = Intent(this,::MainActivity::class.java);
+            startActivity(intent)
+        }
     }
 }
 //private val value = valueOfAnswerToQuestions;
